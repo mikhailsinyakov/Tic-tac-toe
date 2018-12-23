d1ea0bcfba682e1c1254940adc80583d06a6ee2f
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-12-23 19:29:05 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('public'));
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port} ...`);
+});
